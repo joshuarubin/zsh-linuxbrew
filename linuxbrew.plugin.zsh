db0a32773fb8ff26e32d3a1c3951c07ca6086b85
Joshua Rubin <joshuarubin@users.noreply.github.com>
2015-06-09 00:27:43 -0600

    Update linuxbrew.plugin.zsh

--- linuxbrew.plugin.zsh
+++ linuxbrew.plugin.zsh
@@ -1,5 +1,12 @@
 if [[ "$OSTYPE" == linux-gnu && -d "$HOME/.linuxbrew" ]]; then
   path[1,0]="$HOME/.linuxbrew/bin"
   manpath[1,0]="$HOME/.linuxbrew/share/man"
-  infopath[1,0]="$HOME/.linuxbrew/share/info"
+
+  if [[ -z "$INFOPATH" ]]; then
+    INFOPATH="$HOME/.linuxbrew/share/info"
+  else
+    INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+  fi
+
+  export INFOPATH
 fi
