42a1f508a7c274d2b77787c30c3a0cafbca67e73
Joshua Rubin <joshua@rubixconsulting.com>
2015-06-09 03:17:19 -0600

    more PKG_CONFIG_PATH fixes
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

--- linuxbrew.plugin.zsh
+++ linuxbrew.plugin.zsh
@@ -2,9 +2,17 @@ if [[ "$OSTYPE" == linux-gnu && -d "$HOME/.linuxbrew" ]]; then
   path[1,0]="$HOME/.linuxbrew/bin"
   manpath[1,0]="$HOME/.linuxbrew/share/man"
 
+  brew_prefix="$HOME/.linuxbrew"
+
   export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
 
   if [[ -e /usr/bin/pkg-config ]]; then
-    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$(/usr/bin/pkg-config --variable pc_path pkg-config)"
+    export PKG_CONFIG_PATH="$(/usr/bin/pkg-config --variable pc_path pkg-config):$PKG_CONFIG_PATH"
   fi
+
+  if [[ -e $brew_prefix/bin/pkg-config ]]; then
+    export PKG_CONFIG_PATH="$($brew_prefix/bin/pkg-config --variable pc_path pkg-config):$PKG_CONFIG_PATH"
+  fi
+
+  unset brew_prefix
 fi
